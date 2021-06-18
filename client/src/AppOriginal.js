a3839c254e75caf21e785725056bf664d6e9d2b8
dimashaheen <dima.shaheen@student.giu-uni.de>
2021-06-18 16:47:23 +0200

    video4

--- client/src/AppOriginal.js
+++ client/src/AppOriginal.js
@@ -1,17 +1,14 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar';
-import ChangePass from './components/ChangePass';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
-      <ChangePass />
     </div>
-  );    
+  );
 }
 
 export default App;
