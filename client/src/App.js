57ad7f9ed6dca7edc83d72362453a6c62865a143
SohilaArafa <sohila.abourakha@student.giu-uni.de>
2021-06-19 16:46:51 +0200

    commit on "View_Grades"

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar';
 import './App.css';
+import View_Grades from './components/View_Grades';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <View_Grades/>
     </div>
   );
 }
