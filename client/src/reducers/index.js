4927d478c94621122f34de1cd4bdef5059cd1c3f
omarwally <omarwally99@gmail.com>
2021-06-19 17:01:27 +0200

    ...

---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import CourseReducer from './CourseReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
+
+export default combineReducers({
+  course: CourseReducer,
+  error: errorReducer,
+  auth: authReducer
+});
