ee9e8ffc130132afbfe4e3ed8190e63f1e9f2a2c
dimashaheen <dima.shaheen@student.giu-uni.de>
2021-06-19 21:25:14 +0200

    video5

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
+import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ChangePass from './components/ChangePass';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-
-function App() {
+class App extends Component {
+  render (){
   return (
+    <Provider store={store}>
     <div className="App">
       <AppNavbar />
       <ChangePass />
     </div>
+    </Provider>
   );    
-}
+}}
+
+
 
 export default App;
