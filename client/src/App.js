f459541629cf114faf182f02e12afb00a05f4652
omarwally <omarwally99@gmail.com>
2021-06-19 16:46:13 +0200

    index.js in reducers

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar';
 import './App.css';
-
+import {Provider} from 'react-redux';
+import store from './store';
 
 
 function App() {
 
   return (
+    <Provider store={store}> 
     <div className="App">
       <AppNavbar />
     </div>
+    </Provider>
   );
   
 }
