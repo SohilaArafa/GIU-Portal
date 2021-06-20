f90bae3042cd4cae2ebc3cb45b16d23f0e99ecaf
SohilaArafa <sohila.abourakha@student.giu-uni.de>
2021-06-20 17:00:03 +0200

    commit on view grades

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar';
+import TA from './components/TA';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link,
+  // useRouteMatch,
+  // useParams
+} from "react-router-dom";
+import Student from './components/Student';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <AppNavbar />
+      </div>
+
+      <Switch>
+        <Route path="/ta"><TA /></Route>
+        <Route path="/course-students"><CourseStudents /></Route>
+        <Route path="/students"><Student /></Route>
+      </Switch>
+    </Router>
+);
 }
 
 export default App;
