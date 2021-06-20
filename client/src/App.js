f82dc481cfd0c2ca8d5c7d2839b5d29603e3c3f6
farahWatidy <watidyfarah@gmail.com>
2021-06-20 13:29:29 +0200

    commit on view_class_student_list

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,6 @@ function App() {
       <Switch>
         <Route path="/ta"><TA /></Route>
         <Route path="/course-students"><CourseStudents /></Route>
-        <Route path="/students"><Student /></Route>
       </Switch>
     </Router>
 );
