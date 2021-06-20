d4f474498dbd68bd369f71e412f8dc1a78fb95de
SohilaArafa <sohila.abourakha@student.giu-uni.de>
2021-06-20 12:51:30 +0200

    commit on View_Grades

--- routes/api/StudentTakeCourses.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const Course = require('../../models/Course');
-const User = require('../../models/User');
-const router = express.Router();
-const StudentTakeCourses = require('../../models/User');
-
-
-
