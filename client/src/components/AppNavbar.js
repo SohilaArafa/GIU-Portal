afcc44eaad4abaa2816990bb3d5965acb588f5c4
SohilaArafa <sohila.abourakha@student.giu-uni.de>
2021-06-21 18:53:43 +0200

    commit on View_Grade

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> f871736b9dac279b498d22ab88cbce817e7e48b7
 import React, { Component } from 'react';
 import {
     Collapse,
@@ -28,11 +24,7 @@ class AppNavbar extends Component {
             <div>
             <Navbar color="dark" dark expand="sm" className="mb-5">
                 <Container>
-<<<<<<< HEAD
                     <NavbarBrand href="/">ShoppingList</NavbarBrand>
-=======
-                    <NavbarBrand href="/">GIU Dashboard</NavbarBrand>
->>>>>>> f871736b9dac279b498d22ab88cbce817e7e48b7
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
@@ -51,8 +43,4 @@ class AppNavbar extends Component {
 }
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> f871736b9dac279b498d22ab88cbce817e7e48b7
 export default AppNavbar;
