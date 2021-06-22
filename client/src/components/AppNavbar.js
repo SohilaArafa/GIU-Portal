161535ca632309ac5d54d8622ef7cbb8f9af8a04
farahWatidy <watidyfarah@gmail.com>
2021-06-22 11:32:24 +0200

    commit on main

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -25,7 +25,7 @@ class AppNavbar extends Component {
             <div>
             <Navbar color="dark" dark expand="sm" className="mb-5">
                 <Container>
-                    <NavbarBrand href="/">GIU Dashboard</NavbarBrand>
+                    <NavbarBrand href="/">GIU Portal</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
